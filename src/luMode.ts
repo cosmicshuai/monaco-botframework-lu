54e518b4b97bdfa0e14d5e6467d10dedf784fd7d
Shuai Wang <shuwan@microsoft.com>
2019-11-01 16:10:41 +0800

    add token rules

--- src/luMode.ts
+++ src/luMode.ts
@@ -45,16 +45,12 @@ function setupMode(defaults: LanguageServiceDefaultsImpl, modeId: string): (firs
 		],
 		tokenizer: {
 			root:[
-			[/^\s*#\s*\?/, {token: 'QnA', next:'@QnA'}],
+			[/^\s*#\s*\?/, {token: 'QnA', goBack: 1, next:'@QnA'}],
             [/^\s*#/, {token: 'intent', next:'@intent'}],
 			[/^\s*\$/, {token: 'entity-identifier', goBack: 1, next:'@entity_mode'}],
 			[/^\s*@/, {token: 'new-entity-identifier', goBack: 1, next:'@new_entity_mode'}],
 			[/^\s*>\s*[\s\S]*$/, {token: 'comments'}],
 			[/(^\s*\[)1([a-zA-Z0-9._ ]+)(\]\(.{1,2}\/)([a-zA-Z0-9_.*]+)((?:#[a-zA-Z0-9._?]+)?)(\))/,['left_bracket','import-desc','delimeter','file-name','ref-intent','right-parent']],
-			],
-			QnA :[
-				[/.$/,'QnA', '@pop'],
-				[/./,'QnA']
 			],
 
             intent: [
@@ -68,18 +64,38 @@ function setupMode(defaults: LanguageServiceDefaultsImpl, modeId: string): (firs
 				[/^\s*#/, {token: 'intent', next:'@intent'}],
 				[/^\s*\$/, {token: 'entity-identifier', goBack: 1, next:'@entity_mode'}],
 				[/^\s*@/, {token: 'new-entity-identifier', goBack: 1, next:'@new_entity_mode'}],
-				[/\{ (~[\r\n{}] | ('{' ~[\r\n]* }))* \}/,'expression'],
+				[/\{([^\r\n{}]|('{'[^\r\n]*'}'))*\}/,'expression'],
 				[/./, 'normail-intent-string'],
 			],
 
 			new_entity_mode :
 			[
-				[/@\s*(simple|list|regex|prebuilt|composite|ml|patternany|phraselist|intent)/,]
+				[/@\s*((?:[a-z]+)?)\s*(?:([a-zA-Z0-9_\-\|\.\(\)]+)|((?:'|")([a-zA-Z0-9_\-\|\.\(\)\s]+)(?:'|")))\s*(((hasrole)(s)?)?\s*(([^ \t\r\n.,;]+\s*)(,\s*[^ \t\r\n.,;]+)*))?\s*=?\s*((\[([^\r\n{}[()])*\])|(\/([^\r\n])*\/))?/,
+				'new-entity-definition'],
+				[/.$/,'new-entity-end', '@pop']
 			],
 
-			// ml_entity : [
-			// 	[/([a-zA-Z][a-zA-Z0-9_.]*\s+)(=)()/, '']
-			// ],
+			entity_mode : [
+				[/\$\s*(([a-zA-Z0-9_\-\|\.]+)|(\s*))\s*(:)\s*(([a-zA-Z0-9_\-\|\.]+)|(\[([^\r\n{}[()])*)|(\/([^\r\n])*)|(=|,|\!)|(:)|(\s))*/,'entity-line'],
+				[/^\s*-/,{token : 'entity-body-identifier', next: '@entity_body'}]
+				//pop rule
+			],
+
+			entity_body : [
+				[/^\s*-/,{token : 'entity-body-identifier', next: '@entity_body'}],
+				[/\{([^\r\n{}]|('{'[^\r\n]*'}'))*\}/,'expression']
+			],
+
+			QnA : [
+				[/\?((\s)|([^/r/n\t{}]+)*)/,'qnaText'],
+				[/^\s*-((\s)|([^\s\t\r\n{}]))*/,'moreQuestion'],
+				[/^\*\*Filters:\*\*/, {token: 'QnAanswer-Identifier', next:'@QnAanswer'}],
+			],
+
+			QnAanswer : [
+			[/^\s*-\s*((\s)|([^/r/n\t{}]+))*/, 'filterLine'],
+			[/`{3}\s*markdown .*? `{3}/, 'multiline_text'],
+			],
 		}
 	});
 	return worker;
